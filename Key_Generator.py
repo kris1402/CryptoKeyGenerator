b209d17c29ab801867cb4ea7af82e0ae48476465
kris1402 <dlaczegoja4@interia.pl>
2021-11-28 15:44:41 +0100

    Second Commit

--- Key_Generator.py
+++ Key_Generator.py
@@ -1,8 +1,17 @@
 from cryptography.fernet import Fernet
 
+#Write to File
 def test_fun():
     key = Fernet.generate_key()
+    print("Read Key is:")
     print(key)
     file = open ('key.key', 'wb')
     file.write(key) # Type Bytes
     file.close()
+#Read from File
+def test_fun_read():
+    file = open('key.key', 'rb')  # read
+    key = file.read()  # Type Bytes
+    file.close()
+    print("Read Key is:")
+    print(key)
--- main.py
+++ main.py
@@ -69,7 +69,6 @@ def create_csv():
 '''
 
 root = tk.Tk()
-
 canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH)
 canvas.pack()
 
@@ -77,39 +76,30 @@ background_image = tk.PhotoImage(file='background1.png')
 background_label = tk.Label(root, image=background_image)
 background_label.place(relwidth=1, relheight=1)
 
-frame = tk.Frame(root, bg='#779EC6', bd=5)
-frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.2, anchor='n')
-
 frame1 = tk.Frame(root, bg='#779EC6', bd=5)
 frame1.place(relx=0.5, rely=0.32, relwidth=0.75, relheight=0.1, anchor='n')
-#First input
-entry = tk.Entry(frame, font=40)
-entry.place(relwidth=0.45,rely=0.4, relheight=0.45)
-#Second Number
-entry1 = tk.Entry(frame, font=40)
-entry1.place(relx=0.55,rely=0.4,relwidth=0.45, relheight=0.45)
+
 #File name
-entry2 = tk.Entry(frame1, font=30)
-entry2.place(relx=0.25,relwidth=0.4, relheight=1)
+'''entry2 = tk.Entry(frame1, font=30)
+entry2.place(relx=0.25,relwidth=0.4, relheight=1)'''
 
-#button = tk.Button(frame1, text="Get Weather", font=40, command=lambda: test_function(entry.get(),entry1.get()))
-#button.place(relx=0.5, relheight=1, relwidth=0.3,anchor='n')
-label1 = tk.Label(frame1, text = "File Name: ", bg='#779EC6', font = '10')
-label1.place(relx=0.01,rely=0.2, relheight=0.5, relwidth=0.2)
 
-label3 = tk.Label(frame, text = "Upper Range: ", bg='#779EC6', font = '10')
-label3.place(relx=0.03,rely=0.1, relheight=0.3, relwidth=0.22)
 
-label4 = tk.Label(frame, text = "Lower Range: ", bg='#779EC6', font = '10')
-label4.place(relx=0.68,rely=0.1, relheight=0.3, relwidth=0.22, anchor = 'n')
-#########################
-button2 = tk.Button(frame1, text="Generate", font=40, command=lambda: Key_Generator.test_fun())
-button2.place(relx=0.85, relheight=1, relwidth=0.3,anchor = 'n')
-##########################
 lower_frame = tk.Frame(root, bg='#779EC6', bd=10)
 lower_frame.place(relx=0.5, rely=0.45, relwidth=0.75, relheight=0.4, anchor='n')
 
 label = tk.Label(lower_frame)
 label.place(relwidth=1, relheight=1)
+
+#########################
+button2= tk.Button(frame1, text = "Generate", font=40, command=lambda: Key_Generator.test_fun())
+button2.place(relx=0.2,relheight=1, relwidth=0.3,anchor = 'n')
+button2 = tk.Button(frame1, text="Read Key", font=40, command=lambda: Key_Generator.test_fun_read())
+button2.place(relx=0.85, relheight=1, relwidth=0.3,anchor = 'n')
+##########################
+
+
+
+
 
 root.mainloop()
